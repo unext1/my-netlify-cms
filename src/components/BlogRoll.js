4caa1aa0ac51fc65df3ca823d051250769b54af8
Laurynas <laurys235@gmail.com>
2020-04-18 19:51:55 +0200

    remove date

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -13,11 +13,7 @@ class BlogRoll extends React.Component {
         {posts &&
           posts.map(({ node: post }) => (
             <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
+              <article className={`blog-list-item tile is-child box notification`}>
                 <header>
                   {post.frontmatter.featuredimage ? (
                     <div className="featured-thumbnail">
@@ -79,8 +75,6 @@ export default () => (
               frontmatter {
                 title
                 templateKey
-                date(formatString: "MMMM DD, YYYY")
-                featuredpost
                 featuredimage {
                   childImageSharp {
                     fluid(maxWidth: 120, quality: 100) {
