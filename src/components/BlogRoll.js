4aeb5db4627938f152f7962cb49a271365a21204
Laurynas <laurys235@gmail.com>
2020-04-18 20:28:38 +0200

    ok

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -74,6 +74,7 @@ export default () => (
               frontmatter {
                 title
                 templateKey
+                date(formatString: "MMMM DD, YYYY")
                 featuredimage {
                   childImageSharp {
                     fluid(maxWidth: 120, quality: 100) {
