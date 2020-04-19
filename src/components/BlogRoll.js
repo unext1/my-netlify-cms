8d2625785fdc87a2e7708717008a6d18cf1a1f28
Laurynas <laurys235@gmail.com>
2020-04-19 15:15:19 +0200

    main page bg and product css fixed

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -9,40 +9,44 @@ class BlogRoll extends React.Component {
     const { edges: posts } = data.allMarkdownRemark;
 
     return (
-      <div className="columns features">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="column is-4" key={post.id}>
-              <Link to={post.fields.slug}>
-                <div className="card is-shady">
-                  <div className="card-image has-text-centered">
-                    {post.frontmatter.featuredimage ? (
-                      <div className="featured-thumbnail">
-                        <PreviewCompatibleImage
-                          imageInfo={{
-                            image: post.frontmatter.featuredimage,
-                            alt: `featured image thumbnail for post ${post.frontmatter.title}`
-                          }}
-                        />
+      <>
+        <div className="container">
+          <div className="columns">
+            {posts &&
+              posts.map(({ node: post }) => (
+                <div className="column is-4" key={post.id}>
+                  <Link to={post.fields.slug}>
+                    <div className="card is-shady">
+                      <div className="card-image has-text-centered">
+                        {post.frontmatter.featuredimage ? (
+                          <div className="featured-thumbnail">
+                            <PreviewCompatibleImage
+                              imageInfo={{
+                                image: post.frontmatter.featuredimage,
+                                alt: `featured image thumbnail for post ${post.frontmatter.title}`
+                              }}
+                            />
+                          </div>
+                        ) : null}
                       </div>
-                    ) : null}
-                  </div>
 
-                  <div className="card-content">
-                    <div className="content">
-                      <h2> {post.frontmatter.title}</h2>
-                      <div className="subtitle is-size-5"> {post.frontmatter.description}</div>
-                      <p>{post.excerpt}</p>
-                      <p>
-                        <Link to={post.fields.slug}>Learn more</Link>
-                      </p>
+                      <div className="card-content">
+                        <div className="content">
+                          <h2> {post.frontmatter.title}</h2>
+                          <div className="subtitle is-size-5"> {post.frontmatter.description}</div>
+                          <p>{post.excerpt}</p>
+                          <p>
+                            <Link to={post.fields.slug}>Learn more</Link>
+                          </p>
+                        </div>
+                      </div>
                     </div>
-                  </div>
+                  </Link>
                 </div>
-              </Link>
-            </div>
-          ))}
-      </div>
+              ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,69 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from '../components/Layout';
+import Features from '../components/Features';
+import BlogRoll from '../components/BlogRoll';
 
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (
-  <div>
+export const IndexPageTemplate = ({ image, title, heading, subheading, mainpitch, description, intro }) => (
+  <>
     <div
-      className="full-width-image margin-top-0"
+      className="hero-body"
       style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
+        backgroundImage: `url(${!!image.childImageSharp ? image.childImageSharp.fluid.src : image})`,
         backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
+        backgroundAttachment: `fixed`
       }}
     >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
+      <div className="container has-text-centered">
+        <h1 className="title has-text-white">{title}</h1>
+        <h3 className="subtitle has-text-white">{subheading}</h3>
       </div>
     </div>
+
     <section className="section section--gradient">
       <div className="container">
         <div className="section">
@@ -80,9 +38,7 @@ export const IndexPageTemplate = ({
                 </div>
                 <div className="columns">
                   <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
+                    <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
                     <p>{description}</p>
                   </div>
                 </div>
@@ -95,13 +51,11 @@ export const IndexPageTemplate = ({
                   </div>
                 </div>
                 <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
+                  <h3 className="has-text-weight-semibold is-size-2">Our Products</h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
+                    <Link className="btn" to="/products">
+                      See all
                     </Link>
                   </div>
                 </div>
@@ -111,8 +65,8 @@ export const IndexPageTemplate = ({
         </div>
       </div>
     </section>
-  </div>
-)
+  </>
+);
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -122,12 +76,12 @@ IndexPageTemplate.propTypes = {
   mainpitch: PropTypes.object,
   description: PropTypes.string,
   intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
+    blurbs: PropTypes.array
+  })
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
@@ -141,18 +95,18 @@ const IndexPage = ({ data }) => {
         intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
+      frontmatter: PropTypes.object
+    })
+  })
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
@@ -190,4 +144,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
